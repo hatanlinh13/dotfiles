8a646d52d525a5cce0b959dc894d45f81654f661
Ha Tan Linh <mail@hatanlinh.dev>
2024-01-06 11:44:42 +0700

    Update git username

--- nvchad/.config/nvim/lua/custom/plugins.lua
+++ nvchad/.config/nvim/lua/custom/plugins.lua
@@ -20,6 +20,15 @@ local plugins = {
         end,
     },
 
+    {
+        "NvChad/nvim-colorizer.lua",
+        opts = {
+            user_default_options = {
+                names = false,
+            },
+        },
+    },
+
 }
 
 return plugins
